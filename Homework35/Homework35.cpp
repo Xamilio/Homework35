da6e1216f4cb9ad3c7f6b3b477c46965fb82cef6
Sasha Novak <Xamilio25@gmail.com>
2023-11-14 20:47:43 +0200

    Добавьте файлы проекта.

---
+++ Homework35/Homework35.cpp
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+char cript(char c, int k)
+{
+    if (c >= 65 && c <= 90)
+    {
+        c = c - 65;
+        c = (c + k) % 26;
+        if (c < 0)
+        {
+            c = 26 - c;
+        }
+        c = c + 65;
+    }
+    else if (c >= 97 && c <= 122)
+    {
+        c = c - 97;
+        c = (c + k) % 26;
+        if (c < 0)
+        {
+            c = 26 - c;
+        }
+        c = c + 97;
+    }
+    return c;
+}
+int main()
+{
+    const char* pathOut = "Source2.txt";
+    const char* pathIn = "Destination2.txt";
+    FILE* fileOut, * fileIn;
+
+    int k = 0;
+    cout << "Шифрование с использованием ключа: ";
+    cin >> k;
+
+    if (fopen_s(&fileOut, pathOut, "r") != NULL)
+    {
+        cout << "The file cannot be opened";
+    }
+    else if (fopen_s(&fileIn, pathIn, "w") != NULL)
+    {
+        cout << "The file cannot be opened";
+    }
+    else
+    {
+        char c[1];
+        while (fscanf(fileOut, "%c", c) == 1)
+        {
+            c[0] = cript(c[0], k);
+            fprintf(fileIn, "%c", c[0]);
+        }
+        fclose(fileOut);
+        fclose(fileIn);
+    }
+}
